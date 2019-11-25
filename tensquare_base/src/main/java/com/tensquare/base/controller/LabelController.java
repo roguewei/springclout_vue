923d88f25abfb374028e47c60f9f6b206dfe714a
roguewei <1104837637@qq.com>
2019-11-25 10:08:49 +0800

    新增jpa条件查询

--- tensquare_base/src/main/java/com/tensquare/base/controller/LabelController.java
+++ tensquare_base/src/main/java/com/tensquare/base/controller/LabelController.java
@@ -2,9 +2,11 @@ package com.tensquare.base.controller;
 
 import com.tensquare.base.pojo.Label;
 import com.tensquare.base.service.LabelService;
+import entity.PageResult;
 import entity.Result;
 import entity.StatusCode;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -28,7 +30,6 @@ public class LabelController {
     @GetMapping
     public Result findAll(){
         List<Label> labels = labelService.findAll();
-        int i=1/0;
         return new Result(true, StatusCode.OK, "查询成功", labels);
     }
 
@@ -55,6 +56,19 @@ public class LabelController {
     public Result delete(@PathVariable String labelId){
         labelService.delete(labelId);
         return new Result(true, StatusCode.OK, "删除成功");
+    }
+
+    @PostMapping("/search")
+    public Result findSearch(@RequestBody Label label){
+        List<Label> list = labelService.findSearch(label);
+        return new Result(true, StatusCode.OK, "查询成功", list);
+    }
+
+    @PostMapping("/search/{page}/{size}")
+    public Result pageQuery(@RequestBody Label label, @PathVariable int page, @PathVariable int size){
+        Page<Label> pageData = labelService.pageQuery(label, page, size);
+        PageResult<Label> labelPageResult = new PageResult<>(pageData.getTotalElements(), pageData.getContent());
+        return new Result(true, StatusCode.OK, "查询成功", labelPageResult);
     }
 
 }
--- tensquare_base/src/main/java/com/tensquare/base/service/LabelService.java
+++ tensquare_base/src/main/java/com/tensquare/base/service/LabelService.java
@@ -2,11 +2,21 @@ package com.tensquare.base.service;
 
 import com.tensquare.base.dao.LabelDao;
 import com.tensquare.base.pojo.Label;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import util.IdWorker;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -47,4 +57,49 @@ public class LabelService {
         labelDao.deleteById(id);
     }
 
+    /**
+     * @auther: Winston
+     * @Description: 复杂条件查询
+     * @param: root 根对象，把条件封装到哪个对象中，where 类名=label.getId
+     * @param: query 封装查询关键字，比如group by order by等，基本不用，直接写在sql语句中
+     * @param: cb 封装条件对象
+     * @return:
+     * @date: 2019/11/24 13:47
+     */
+    public List<Label> findSearch(Label label) {
+        return labelDao.findAll(getSpecification(label));
+    }
+
+    public Page<Label> pageQuery(Label label, int page, int size) {
+        Pageable pageable = PageRequest.of(page-1, size);
+        Page<Label> labelDaoAll = labelDao.findAll(getSpecification(label), pageable);
+        return labelDaoAll;
+    }
+
+    private Specification<Label> getSpecification(Label label) {
+        return new Specification<Label>() {
+            @Override
+            public Predicate toPredicate(Root<Label> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
+                // 封装参数
+                List<Predicate> list = new ArrayList<>();
+                if (StringUtils.isNotBlank(label.getLabelname())) {
+                    // where labelname like %林
+                    Predicate predicate = cb.like(root.get("labelname").as(String.class), label.getLabelname() + "%");
+                    list.add(predicate);
+                }
+                if (StringUtils.isNotBlank(label.getState())) {
+                    // where state = "1
+                    Predicate predicate = cb.equal(root.get("state").as(String.class), label.getState());
+                    list.add(predicate);
+                }
+
+                // new一个数组作为最终返回值的条件
+                Predicate[] parr = new Predicate[list.size()];
+                // 把list直接转成数组
+                parr = list.toArray(parr);
+                // 直接返回null表示不需要任何条件
+                return cb.and(parr); // where labelname like "%林" and state = "1"
+            }
+        };
+    }
 }
